cdfac58e393ccddf7e0aecfd0ebe08c4ed149bfc
Masaya Yamamoto <masaya1122baseball@gmail.com>
2023-07-22 16:32:43 +0900

    Add: daisyUIをインストール、設定を追加

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -14,10 +14,28 @@ module.exports = {
       },
     },
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+        "primary": "#9ca3af",
+        "secondary": "#e11d48",
+        "accent": "#5c7f67",
+        "neutral": "#0284C7",
+        "base-100": "#f3f4f6",
+        "info": "#3abff8",
+        "success": "#36d399",
+        "warning": "#fbbd23",
+        "error": "#f87272",
+        },
+      },
+    ],
+  },
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/container-queries'),
-  ]
+    require("daisyui"),
+  ],
 }
