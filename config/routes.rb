8c01c53075748bebeabaf06ce34defcf572b6d3e
Masaya Yamamoto <masaya1122baseball@gmail.com>
2023-08-03 11:28:29 +0900

    Add: rails g controller user_sessions new create destroyを実行

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'user_sessions/new'
+  get 'user_sessions/create'
+  get 'user_sessions/destroy'
   root 'static_pages#top'
   resources :users, only: [:new, :create]
 end
