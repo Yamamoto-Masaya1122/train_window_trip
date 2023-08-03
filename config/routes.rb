3f269140d815dfcce813c5585abbde638600c0ed
Masaya Yamamoto <masaya1122baseball@gmail.com>
2023-08-03 12:52:48 +0900

    Add: ルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'user_sessions/new'
-  get 'user_sessions/create'
-  get 'user_sessions/destroy'
   root 'static_pages#top'
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
   resources :users, only: [:new, :create]
 end
