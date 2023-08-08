93e772150e6c4850fc2dc146b4d5f510e109c5f6
Masaya Yamamoto <masaya1122baseball@gmail.com>
2023-08-08 23:32:33 +0900

    Add: Categoryモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_08_142452) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_08_143058) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "lines", force: :cascade do |t|
     t.string "name", null: false
