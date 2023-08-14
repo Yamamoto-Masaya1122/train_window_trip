b441e0e67dc96f6dd364291773082de57be0f80f
Masaya Yamamoto <masaya1122baseball@gmail.com>
2023-08-14 23:22:08 +0900

    Fix: 検索条件に合う路線が表示されるように検索機能を修正

--- app/models/prefecture_line.rb
+++ app/models/prefecture_line.rb
@@ -1,4 +1,8 @@
 class PrefectureLine < ApplicationRecord
   belongs_to :prefecture
   belongs_to :line
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "id", "line_id", "prefecture_id", "updated_at"]
+  end
 end
