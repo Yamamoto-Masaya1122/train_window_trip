3282708fbfdfd72ed0e72609232f34ed2908a7d5
Masaya Yamamoto <masaya1122baseball@gmail.com>
2023-08-15 23:31:10 +0900

    Fix: Youtube_searchsコントローラを修正

--- app/controllers/youtube_searchs_controller.rb
+++ app/controllers/youtube_searchs_controller.rb
@@ -3,28 +3,23 @@ require 'active_support/all'
 class YoutubeSearchsController < ApplicationController
 
   def index
-    # 動画検索に使う路線名とカテゴリをセット
-    line_name =  "信楽高原鐵道" || " "
-    category =  nil || " "
-    # キーワードがタイトルと完全一致する動画のみを表示するために使う
-    @keyword = "信楽高原鐵道" || " "
-    @category = nil
-
-    @youtube_data = find_videos(line_name, category)
+    @youtube_data = find_videos(params[:format])
   end
   
-  def find_videos(keyword, category, after: 3.years.ago, before: Time.now)
+  def find_videos(keyword, after: 4.years.ago, before: Time.now)
     service = Google::Apis::YoutubeV3::YouTubeService.new
     # Herokuにデプロイする用
     service.key = ENV['api_key']
-
+    # キーワードがタイトルと完全一致する動画のみを表示するために使う
+    @keyword = keyword
+    
     @youtube_data = []
 
     # 検索結果を取得
     search_results = service.list_searches(
       :snippet,
       type: "video",
-      q: keyword + " " +  "前面展望" + " " + category,
+      q: keyword + " " +  "前面展望",
       max_results: 24,
       # HD 動画のみ
       video_definition: "high",
