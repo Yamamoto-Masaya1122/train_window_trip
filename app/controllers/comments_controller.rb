ae50fb86ec7960c5b2881f977cc1fa0413104bd0
Masaya Yamamoto <masaya1122baseball@gmail.com>
2023-08-22 00:42:43 +0900

    Add: コメント投稿と削除をAjax化

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,24 +1,53 @@
 class CommentsController < ApplicationController
 
   def create
-    comment = current_user.comments.build(comment_params)
+    @commentable = find_commentable
+    @comment = @commentable.comments.build(comment_params)
 
-    if comment.save
-      redirect_to search_result_path(params[:search_result_id]), success: 'コメントしました'
-    else
-      redirect_to search_result_path(params[:search_result_id]), error: 'コメントできませんでした'
+    respond_to do |format|
+      if @comment.save
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.prepend('comments', partial: 'comments/comment', locals: { comment: @comment }),
+            turbo_stream.replace('comment_form', partial: 'comments/form', locals: { commentable: @commentable, comment: Comment.new })
+          ]
+        end
+        format.js
+        format.html { redirect_to @comment.commentable, flash: { success: 'コメントしました' } }
+      else
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.replace('comment_form', partial: 'comments/form', locals: { commentable: @comment.commentable, comment: @comment }),
+            turbo_stream.prepend('error_messages', partial: 'shared/error_messages', locals: { object: @comment }),
+          ]
+        end
+        format.html { redirect_to @comment.commentable, flash: { error: 'コメントできませんでした' } }
+      end
     end
   end
 
   def destroy
-    @comment = current_user.comments.find(params[:search_result_id])
-    @comment.destroy!
-    redirect_to search_result_path(params[:id]), success: 'コメントを削除しました'
+    @comment = current_user.comments.find(params[:id])
+    @comment.destroy
+    respond_to do |format|
+        format.turbo_stream { render turbo_stream: turbo_stream.remove(@comment) }
+        format.js
+        format.html { redirect_to @comment.commentable, flash: { success: 'コメントを削除しました' } }
+    end
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:body).merge(line_id: params[:search_result_id])
+    params.require(:comment).permit(:body).merge(user_id: current_user.id)
+  end
+
+  def find_commentable
+    params.each do |name, value|
+      if name =~ /(.+)_id$/
+        return $1.classify.constantize.find(value)
+      end
+    end
+    nil
   end
 end
--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -1,7 +1,15 @@
 class LinesController < ApplicationController
+  skip_before_action :require_login, only: %i[index show]
 
   def index
-    @lines = Line.all
+    @line = Line.ransack(params[:q])
+    @lines = @line.result
   end
 
+  def show
+    @line = Line.find(params[:id])
+    @comment = Comment.new
+    @comments = @line.comments.includes(:user).order(created_at: :desc)
+    @commentable = @line
+  end
 end
