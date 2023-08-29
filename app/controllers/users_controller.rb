d62869236c7952dd95eba029473e6d2a9201fce7
Masaya Yamamoto <masaya1122baseball@gmail.com>
2023-08-29 23:07:20 +0900

    Fix: users_controllerのフラッシュメッセージを国際化

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to login_path, success: 'ユーザー登録が完了しました'
+      redirect_to login_path, success: t('.success')
     else
-      flash.now[:error] = 'ユーザー登録に失敗しました'
+      flash.now[:error] = t('.fail')
       render :new, status: :unprocessable_entity
     end
   end
