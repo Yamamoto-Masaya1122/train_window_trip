772c5091be864c3bfcd33ebe50937d56b7a8ff37
Masaya Yamamoto <masaya1122baseball@gmail.com>
2023-08-31 14:04:57 +0900

    Fix: seeds.rbファイルを修正

--- db/seeds.rb
+++
@@ -1,5 +0,0 @@
-require "./db/fixtures/category.rb"
-require "./db/fixtures/line_category.rb"
-require "./db/fixtures/line.rb"
-require "./db/fixtures/prefecture_line.rb"
-require "./db/fixtures/prefecture.rb"
