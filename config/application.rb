55de82821253c9af4a5378c435bc51b197ea12e8
Masaya Yamamoto <masaya1122baseball@gmail.com>
2023-09-13 19:03:25 +0900

    Add: タイムゾーンを日本に変更

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module TrainWindowTrip
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
     config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
     # Configuration for the application, engines, and railties goes here.
     #
