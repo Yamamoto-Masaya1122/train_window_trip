022c672207327fe3e13d4eb0927902ea490d1504
Masaya Yamamoto <masaya1122baseball@gmail.com>
2023-09-18 11:49:47 +0900

    Fix: 検索機能を路線一覧ページに移動

--- app/models/prefecture.rb
+++ app/models/prefecture.rb
@@ -1,4 +1,12 @@
 class Prefecture < ApplicationRecord
   has_many :prefecture_lines, dependent: :destroy
   has_many :lines, through: :prefecture_lines
+
+  def self.ransackable_attributes(*)
+    ["created_at", "id", "name", "updated_at"]
+  end
+
+  def self.ransackable_associations(*)
+    ["lines", "prefecture_lines"]
+  end
 end
