5fdfc0589ce0761ed0e5aae238e07619d912031b
Masaya Yamamoto <masaya1122baseball@gmail.com>
2023-09-23 00:59:00 +0900

    Fix: resourcesメソッドのonlyの書き方を%i[]で統一

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,11 @@ Rails.application.routes.draw do
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
-  resources :users, only: [:new, :create]
-  resources :youtube_searchs, only: [:index]
-  resource :profile, only: [:show, :edit, :update]
+  resources :users, only: %i[new create]
+  resources :youtube_searchs, only: %i[index]
+  resource :profile, only: %i[show edit update]
   resources :lines, only: %i[index show] do
     resources :comments
-    resources :likes, only: %i[create destroy]
     collection do
       get :update_lines_options
     end
