2189b11ae8dc9a80b8191f430ee1d2bd963218ab
Masaya Yamamoto <masaya1122baseball@gmail.com>
2023-10-12 11:03:09 +0900

    Improve: いいねボタンをAjaxに変更

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,12 +4,10 @@ class LikesController < ApplicationController
   def create
     @line = Line.find(params[:line_id])
     current_user.line_like(@line)
-    redirect_to request.referer
   end
 
   def destroy
     @line = current_user.lines.find(params[:id])
     current_user.unlikes_line(@line)
-    redirect_to request.referer
   end
 end
