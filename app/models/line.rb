2556bb7cc8c683cc26f40c0dfda916aa457fe822
Masaya Yamamoto <masaya1122baseball@gmail.com>
2023-10-14 12:09:17 +0900

    Add: Videoモデルのアソシエーションを追加

--- app/models/line.rb
+++ app/models/line.rb
@@ -5,7 +5,8 @@ class Line < ApplicationRecord
   has_many :categories, through: :line_categories
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
-  has_many :users, through: :likes 
+  has_many :users, through: :likes
+  has_many :videos, dependent: :destroy
   
   def self.ransackable_attributes(auth_object = nil)
     ["created_at", "description", "id", "image", "line_url", "name", "recommended_train_window_spot", "updated_at"]
