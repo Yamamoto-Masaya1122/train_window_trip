2f411dec291ac5f1b0c709d5e7951c8b5f220b56
Masaya Yamamoto <masaya1122baseball@gmail.com>
2023-11-01 21:51:35 +0900

    Fix: metaタグのデフォルトの設定を変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,11 @@ module ApplicationHelper
   def default_meta_tags
     {
       site: 'Train Window Trip',
-      title: 'いつでもどこでも電車旅行を体感できるサービス',
+      title: 'いつでもどこでも電車で旅行する体験ができるサービス',
       reverse: true,
       charset: 'utf-8',
-      description: 'Train Window Tripを使えば「都道府県・カテゴリ・路線」を選択することで美しい車窓のスポット情報を検索できます。',
-      keywords: '美しい車窓,車窓,Train Window Trip',
+      description: 'Train Window Tripは電車で旅行する体験と美しい車窓のスポット情報を提供するサービスです。',
+      keywords: '美しい車窓,車窓,電車',
       canonical: request.original_url,
       separator: '|',
       og: {
