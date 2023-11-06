1b475f669e0a1f00e8e9cea5c6837e60e4acc165
Masaya Yamamoto <masaya1122baseball@gmail.com>
2023-11-06 23:16:15 +0900

    Add: パスワードリセット機能を国際化

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -12,7 +12,7 @@ class PasswordResetsController < ApplicationController
   def create
     @user = User.find_by(email: params[:email])
     @user&.deliver_reset_password_instructions!
-    redirect_to login_path, success: 'パスワードリセット手順を送信しました'
+    redirect_to login_path, success: t('.success')
   end
 
   def update
@@ -22,9 +22,10 @@ class PasswordResetsController < ApplicationController
 
     @user.password_confirmation = params[:user][:password_confirmation]
     if @user.change_password(params[:user][:password])
-      redirect_to login_path, success: 'パスワードを変更しました'
+      redirect_to login_path, success: t('.success')
     else
-      render :edit
+      flash.now[:danger] = t('.fail')
+      render :edit, status: :unprocessable_entity
     end
   end
 end
