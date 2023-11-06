9a84dbb084ca083cc5f9a3533562f847830bd832
Masaya Yamamoto <masaya1122baseball@gmail.com>
2023-11-06 22:32:41 +0900

    Improve: Lintチェックに引っかかった箇所を改善

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,37 +1,26 @@
 class PasswordResetsController < ApplicationController
   skip_before_action :require_login
-	
-	#パスワードリセット申請ページを表示する
+
   def new; end
 
-	#ユーザーがパスワード再設定フォームに電子メールを入力し、送信したときにここに表示されます。
-  def create
-    @user = User.find_by_email(params[:email])
-
-	#パスワードをリセットして、メールをユーザーに送信する。（メールにはランダムなトークンを含むURLが入ってる）
-    @user.deliver_reset_password_instructions! if @user
-
-	#メールが見つかったかどうかに関わらず、ユーザーにメールが送信がされたことを伝える。
-	#これは、どのメールがシステム内に存在するかという情報を攻撃者に漏らさないためである
-    redirect_to login_path, success: 'パスワードリセット手順を送信しました'
-  end
-
-	#パスワード再設定フォームです
   def edit
     @token = params[:id]
     @user = User.load_from_reset_password_token(params[:id])
-    return not_authenticated if @user.blank?
+    not_authenticated if @user.blank?
   end
 
-	#このアクションはユーザがパスワード再設定フォームを送信したときに発生する
+  def create
+    @user = User.find_by(email: params[:email])
+    @user&.deliver_reset_password_instructions!
+    redirect_to login_path, success: 'パスワードリセット手順を送信しました'
+  end
+
   def update
     @token = params[:id]
     @user = User.load_from_reset_password_token(params[:id])
     return not_authenticated if @user.blank?
 
-	#フォームで入力した値をレコードに代入することで更新している
     @user.password_confirmation = params[:user][:password_confirmation]
-
     if @user.change_password(params[:user][:password])
       redirect_to login_path, success: 'パスワードを変更しました'
     else
--- db/schema.rb
+++ db/schema.rb
@@ -87,7 +87,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_06_053902) do
     t.datetime "reset_password_email_sent_at"
     t.integer "access_count_to_reset_password_page", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "videos", force: :cascade do |t|
