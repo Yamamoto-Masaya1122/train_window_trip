6e118e9ef9735f4f272521caf9cf5f598dda5319
Masaya Yamamoto <masaya1122baseball@gmail.com>
2023-11-08 20:37:34 +0900

    Fix: Gmailの設定を修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,17 +66,16 @@ Rails.application.configure do
   config.action_mailer.default_url_options = Settings.default_url_options.to_h
   config.action_mailer.delivery_method = :smtp
 
-  if Rails.application.credentials.mail
-    config.action_mailer.smtp_settings = {
-      address: 'smtp.gmail.com',
-      domain: 'gmail.com',
-      port: '587',
-      user_name: ENV['GMAIL_ADDRESS'],
-      password: ENV['GMAIL_PASSWORD'],
-      authentication: :plain,
-      enable_starttls_auto: true
-    }
-  end
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    domain: 'gmail.com',
+    port: '587',
+    user_name: ENV['GMAIL_ADDRESS'],
+    password: ENV['GMAIL_PASSWORD'],
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
