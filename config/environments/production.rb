70a2e144a574d4996281ccaffbf6d98be37dd5f5
Masaya Yamamoto <masaya1122baseball@gmail.com>
2023-11-08 18:09:42 +0900

    Fix: パスワードリセットメールをGmailで送信できるように設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,20 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "train_window_trip_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = Settings.default_url_options.to_h
+  config.action_mailer.delivery_method = :smtp
 
+  if Rails.application.credentials.mail
+    config.action_mailer.smtp_settings = {
+      address: 'smtp.gmail.com',
+      domain: 'gmail.com',
+      port: '587',
+      user_name: ENV['GMAIL_ADDRESS'],
+      password: ENV['GMAIL_PASSWORD'],
+      authentication: :plain,
+      enable_starttls_auto: true
+    }
+  end
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
@@ -90,5 +103,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = Settings.default_url_options.to_h
 end
