255f6a26fee7ea54ef2a630ebac7704e85db07db
Masaya Yamamoto <masaya1122baseball@gmail.com>
2023-11-13 20:55:01 +0900

    Add: 管理画面ユーザー詳細、編集、削除の作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,25 +5,31 @@ class Admin::UsersController < Admin::BaseController
     @users = User.all
   end
 
-  def show
-    
-  end
+  def show; end
 
-  def edit
-
-  end
+  def edit; end
 
   def update
-
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user), success: t('defaults.message.updated', item: User.model_name.human)
+    else
+      flash.now['danger'] = t('defaults.not_updated', item: User.model_name.human)
+      render :edit
+    end
   end
 
   def destroy
-
+    @user.destroy!
+    redirect_to admin_users_path, success: t('defaults.message.deleted', item: User.model_name.human), status: :see_other
   end
 
   private
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :avatar, :avatar_cache, :role)
   end
 end
